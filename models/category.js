4f34b069c6b49dcc2fd4e5ad078ed7161a13fbe5
zarose0000ds <zarose0000ds@gmail.com>
2021-10-27 15:54:12 +0800

    feat: add category and record data seeder

--- models/category.js
+++ models/category.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const categorySchema = new mongoose.Schema({
   id: { type: Number, require: true },
-  name: { type: String, require: true }
+  name: { type: String, require: true },
+  icon: { type: String }
 })
 
 module.exports = mongoose.model('Category', categorySchema)
---
+++ models/seeds/categorySeeder.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const db = require('../../config/mongoose')
+
+const Category = require('../category')
+
+const CATEGORY = {
+  家居物業: 'https://fontawesome.com/icons/home?style=solid',
+  交通出行: 'https://fontawesome.com/icons/shuttle-van?style=solid',
+  休閒娛樂: 'https://fontawesome.com/icons/grin-beam?style=solid',
+  餐飲食品: 'https://fontawesome.com/icons/utensils?style=solid',
+  其他: 'https://fontawesome.com/icons/pen?style=solid'
+}
+const CATEGORY_NAMES = Object.keys(CATEGORY)
+
+db.once('open', () => {
+  const categories = []
+
+  CATEGORY_NAMES.forEach((name, id) => {
+    const category = {
+      id: id + 1,
+      name,
+      icon: CATEGORY[name]
+    }
+    categories.push(category)
+  })
+
+  Category.create(categories).then(() => {
+    console.log('Category done')
+    process.exit()
+  }).catch(e => console.log(e))
+})
