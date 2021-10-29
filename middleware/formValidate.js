148b0f2a4e318048355bc0ead9a5850d1e33528c
zarose0000ds <zarose0000ds@gmail.com>
2021-10-29 13:54:46 +0800

    feat: add creating new record feature including server-side validation

---
+++ middleware/formValidate.js
@@ -0,0 +1,34 @@
+module.exports = {
+  recordValidator: (failureRedirect) => {
+    return (req, res, next) => {
+      const { name, date, categoryId, amount } = req.body
+      const dateNow = Date.now()
+      const errors = []
+
+      if (!name || !date || !categoryId || !amount) {
+        errors.push({ message: '請填寫所有必填項目！' })
+      }
+      if (date && Date.parse(date) > dateNow) {
+        errors.push({ message: '日期不得超過今天！' })
+      }
+      if (amount && Number(amount) <= 0) {
+        errors.push({ message: '金額需大於0！' })
+      }
+      if (errors.length > 0) {
+        // FOR CATEGORY SELECT OPTIONS
+        return require('../models/category').find().lean().sort({ id: 'asc' }).then(category => {
+          // FOR RETURNING CURRENT SELECTED CATEGORY
+          for (let i = 0; i < category.length; i++) {
+            if (category[i].id === Number(categoryId)) {
+              category[i].selected = 'selected'
+              break
+            }
+          }
+          res.render('new', { errors, category, record: req.body })
+        }).catch(e => console.log(e))
+      }
+
+      next()
+    }
+  }
+}
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,5 +1,32 @@
 const router = require('express').Router()
 
+const { recordValidator } = require('../../middleware/formValidate')
 
+const Category = require('../../models/category')
+const Record = require('../../models/record')
+
+router.get('/new', (req, res) => {
+  Category.find().lean().sort({ id: 'asc' }).then(category => res.render('new', { category })).catch(e => console.log(e))
+})
+
+router.post('/new', recordValidator('new'), (req, res) => {
+  const userId = req.user.id
+  const { name, date, categoryId, amount } = req.body
+
+  Record.estimatedDocumentCount().then(count => {
+    const id = count + 1
+    const dateObject = new Date(date + 'T12:00:00.000+08:00')
+    return Record.create({ id, name, date: dateObject, amount, userId, categoryId }).then(() => res.redirect('/'))
+  }).catch(e => console.log(e))
+})
+
+router.get('/:id/edit', (req, res) => {
+  const userId = req.user.id
+  const id = req.params.id
+
+  Record.findOne({ id, userId }).lean().then(record => {
+    res.render('edit', { record })
+  }).catch(e => console.log(e))
+})
 
 module.exports = router
